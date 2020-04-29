6320f68e0c7bbc458139cf9ed627c78a35eb163a
femoiseev <femoiseev@gmail.com>
2020-04-29 20:52:49 +0200

    Fixed json access in production

--- website/src/scripts/skills.js
+++ website/src/scripts/skills.js
@@ -2,51 +2,39 @@ import * as d3 from "d3";
 import { RadarChart } from "./radar_chart";
 import { DefaultDict } from "./helpers";
 
+const coursePrograms = require("../processed_data/course_programs.json");
+
 export function showSkills(data) {
   d3.select("#course").attr("width", "0");
   d3.select("#plot").attr("width", "0");
 
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "src/processed_data/course_programs.json", true);
-  xhr.responseType = "blob";
-  xhr.onload = function (e) {
-    if (this.status == 200) {
-      var file = new File([this.response], "temp");
-      var fileReader = new FileReader();
-      fileReader.addEventListener("load", function () {
-        const res = JSON.parse(fileReader.result);
-        showSkillsForPrograms(data, res);
-      });
-      fileReader.readAsText(file);
-    }
-  };
-  xhr.send();
+  showSkillsForPrograms(data, coursePrograms);
 }
 
-function showSkillsForPrograms(data, course_programs, programs_to_show = 7) {
+function showSkillsForPrograms(data, coursePrograms, programsToShow = 7) {
   let programs = new DefaultDict(0);
 
   data.forEach((course) => {
-    const course_name = course.name.toLowerCase();
-    if (course_name in course_programs) {
-      course_programs[course_name].forEach((program) => {
+    const courseName = course.name.toLowerCase();
+    if (courseName in coursePrograms) {
+      coursePrograms[courseName].forEach((program) => {
         programs[program] += course.credits;
       });
     } else {
-      console.log(course_name);
+      console.log(courseName);
     }
   });
 
   programs = Object.entries(programs);
   programs.sort((a, b) => (a[1] < b[1] ? 1 : -1));
-  programs = programs.slice(0, programs_to_show);
+  programs = programs.slice(0, programsToShow);
   console.log(programs);
 
   var margin = { top: 50, right: 80, bottom: 50, left: 80 },
     width = Math.min(700, window.innerWidth / 4) - margin.left - margin.right,
     height = Math.min(width, window.innerHeight - margin.top - margin.bottom);
 
-  var radar_data = [
+  var radarData = [
     {
       name: "All skills",
       axes: [],
@@ -66,7 +54,7 @@ function showSkillsForPrograms(data, course_programs, programs_to_show = 7) {
   ];
 
   programs.forEach((program) => {
-    radar_data[0].axes.push({
+    radarData[0].axes.push({
       axis: prettifyProgramName(program[0]),
       value: program[1],
     });
@@ -82,7 +70,7 @@ function showSkillsForPrograms(data, course_programs, programs_to_show = 7) {
     format: ".0f",
   };
 
-  RadarChart(".radarChart", radar_data, radarChartOptions);
+  RadarChart(".radarChart", radarData, radarChartOptions);
 }
 
 function prettifyProgramName(program) {
