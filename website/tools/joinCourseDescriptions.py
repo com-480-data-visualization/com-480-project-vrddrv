2521e8a79a0d2398da1b366680a6e16917f39bd8
ottogin <lukartoil@gmail.com>
2020-04-30 22:35:32 +0200

    NEW: collect all classes secriptions in one file

---
+++ website/tools/joinCourseDescriptions.py
@@ -0,0 +1,29 @@
+import json
+import os
+import random
+from tqdm import tqdm
+
+INPUT_FOLDER = 'src/scraped_data'
+OUTPUT_PATH = 'src/processed_data/course_descriptions.json'
+
+if __name__ == "__main__":
+    files = []
+    for (dirpath, dirnames, filenames) in os.walk(INPUT_FOLDER):
+        files += [os.path.join(dirpath, file)
+                  for file in filenames if file[-5:] == '.json']
+
+    classes = {}
+    for file_path in tqdm(files):
+        with open(file_path, 'r') as json_data:
+            for course in json.load(json_data):
+
+                # Generate fake data:
+                course["gpa_histogram"] = [
+                    random.randint(0, 100) for i in range(8)]
+                course["avg_gpa_year"] = [
+                    random.random() * 2 + 4 for i in range(3)]
+
+                classes[course["courseName"].lower()] = course
+
+    with open(OUTPUT_PATH, 'w') as outfile:
+        json.dump(classes, outfile, indent=2)
