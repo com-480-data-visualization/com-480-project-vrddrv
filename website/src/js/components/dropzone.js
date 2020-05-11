20db9e621d789b2635ede667b52f6cb76d9e7a90
ottogin <lukartoil@gmail.com>
2020-05-11 14:56:38 +0200

    FIX: change opacity, delete rudement code

--- website/src/js/components/dropzone.js
+++ website/src/js/components/dropzone.js
@@ -4,15 +4,8 @@ import React from "react";
 
 export function DropZone(props) {
   const [showBox, setShowBox] = React.useState(true);
+  const [opacity, setOpacity] = React.useState(1.0);
 
-  const handleDragEnter = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-  };
-  const handleDragLeave = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-  };
   const handleDragOver = (e) => {
     e.preventDefault();
     e.stopPropagation();
@@ -29,42 +22,11 @@ export function DropZone(props) {
       className="DropZone"
       onDrop={handleDrop}
       onDragOver={handleDragOver}
-      onDragEnter={handleDragEnter}
-      onDragLeave={handleDragLeave}
-      style={{ display: showBox ? "block" : "none" }}
+      onMouseOver={() => setOpacity(0.8)}
+      onMouseLeave={() => setOpacity(1.0)}
+      style={{ display: showBox ? "block" : "none", opacity: opacity }}
     >
       <h1>Drag and drop your transcript here!</h1>
     </div>
   );
-};
-
-// export class DropZone {
-//   constructor(canvasWidth, canvasHeight, dropHandler) {
-//     let dropZone = d3.select("svg#plot").append("g").attr("id", "drop_zone");
-
-//     dropZone
-//       .append("rect")
-//       .attr("x", 0)
-//       .attr("y", 0)
-//       .attr("width", canvasWidth)
-//       .attr("height", canvasHeight);
-
-//     dropZone
-//       .append("text")
-//       .attr("x", canvasWidth / 2)
-//       .attr("y", canvasHeight / 2)
-//       .text("Drop your Transcript here");
-
-//     dropZone
-//       .on("drop", dropHandler)
-//       .on("dragover", () => {
-//         d3.event.preventDefault(); // Prevents file from being opened on drop
-//       })
-//       .on("mouseover", function (d, i) {
-//         d3.select(this).attr("opacity", "0.8");
-//       })
-//       .on("mouseout", function (d, i) {
-//         d3.select(this).attr("opacity", "1.0");
-//       });
-//   }
-// }
+}
